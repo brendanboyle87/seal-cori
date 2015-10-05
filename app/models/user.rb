97490d12eaf73e5dd67a14f4bbec2e99bfee3ee4
Brendan Boyle <brendanboyle87@gmail.com>
2015-10-05 16:30:25 -0400

    add terms of service

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :terms_of_service, acceptance: true
   validates :zip, numericality: { only_integer: true }, allow_nil: true
   validates :social_security, numericality: { only_integer: true }, allow_nil: true
   validates :zip, length: {is: 6}, allow_nil: true
