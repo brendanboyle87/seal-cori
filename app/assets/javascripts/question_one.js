f4be02d3f7e2e8183c9d7e8825b0d591d93fa30c
Brendan Boyle <brendanboyle87@gmail.com>
2015-10-06 18:51:38 -0400

    form disappears on submission

--- app/assets/javascripts/question_one.js
+++ app/assets/javascripts/question_one.js
@@ -1,11 +1,12 @@
 $('#question_one_submit').on('click', function(e) {
       e.preventDefault();
+
       if (document.getElementById("yes_or_no_no").checked) {
-        document.location.href = '/pages/nocori'
+        document.location.href = '/pages/nocori';
       }
-  });    
-        // $.get( "/pages/nocori", function() {
-        //     location.href = ""
-        //     });
-        //   }
-        // });
+      if (document.getElementById("yes_or_no_yes").checked) {
+        debugger;
+        $(".question-box").hide();
+        console.log("hidden");
+      }
+  });
