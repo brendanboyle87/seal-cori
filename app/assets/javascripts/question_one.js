e6c847d1d730549d98c98866d5a6ad9341cdeb16
Brendan Boyle <brendanboyle87@gmail.com>
2015-10-06 19:18:45 -0400

    feed the hound

--- app/assets/javascripts/question_one.js
+++ app/assets/javascripts/question_one.js
@@ -1,12 +1,10 @@
 $('#question_one_submit').on('click', function(e) {
       e.preventDefault();
-
-      if (document.getElementById("yes_or_no_no").checked) {
+      if (document.getElementById('yes_or_no_no').checked) {
         document.location.href = '/pages/nocori';
       }
-      if (document.getElementById("yes_or_no_yes").checked) {
-        debugger;
-        $(".question_one").hide();
-        console.log("hidden");
+      if (document.getElementById('yes_or_no_yes').checked) {
+        $('#question_one').hide();
+        console.log('hidden');
       }
   });
