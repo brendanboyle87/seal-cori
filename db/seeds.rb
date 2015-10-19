b46af42115ec074ce9cc95712c33cb0db600a23a
Brendan Boyle <brendanboyle87@gmail.com>
2015-10-19 15:54:19 -0400

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ unsealable_offenses = [ "Firearm Dealer's License, Improper Issue", "Unlicensed 
   "Firearm, Alien Possess", "Perjury", "False Statement to State Agency", "False Return by Process Server", "False Report by Public Employee",
   "Bribe or Attempt to Bribe", "Court/Judge/Juror Picketing", "Witness, Juror, Police, Court Official, Intimidation", "Disrupt Court Proceedgings",
   "Withhold Evidence from Criminal Proceeding", "Juror Accept Bribe", "Witness, Employer Discharge", "Aid Felon Escape", "Aid Misdemeanant Escape",
-  "Escape from Municiap Lockup", "Escape from Officer", "Escape from Penal Instituion", "Prison Guard Have Sex Relations with Prisoner", "Police Officer, Fail Assist"
+  "Escape from Municiap Lockup", "Escape from Officer", "Escape from Penal Instituion", "Prison Guard Have Sex Relations with Prisoner", "Police Officer, Fail Assist",
   "Justice of Peace, Fail Assist", "Deliver Liquor to Prisoner", "Deliver Drugs to Prisoner", "Prisoner Bring Weapon or Drugs from Furlough",
   "False Alarm From Police Call Box", "Interfere with Firefighter", "Resist Arrest", "Impersonate Police Officer or Public Official", "False Name, Social Security to Police",
   "False Report of Motor Vehicle Theft", "Bribe Public Employee", "State Employee Imporper Compensation as Agent/Atty", "State Employee, Conflict of Interest",
